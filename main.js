42f25a8e4ede5616e101beaaf97f526759efc5ea
RobStenning <46462192+RobStenning@users.noreply.github.com>
2022-01-18 21:46:47 +0000

    added devtools, reload + project code window

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
-
+let addWindow;
 //listen for app ready
 app.on('ready', function() {
     //create new window
@@ -16,6 +16,11 @@ app.on('ready', function() {
         protocol: 'file',
         slashes: true
     }));
+    //quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
     //build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     //insert menu
@@ -23,11 +28,37 @@ app.on('ready', function() {
 });
 
 
+//create project code window
+function createprojectCodeWindow(){
+    //create new window
+    projectCodeWindow = new BrowserWindow({
+        width: 350,
+        height: 150,
+        title: 'Specify the project code'    
+    });
+    //load html file
+    projectCodeWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'projectCodeWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    //garbage collection, saves memory?
+    projectCodeWindow.on('close', function(){
+        addWindow = null;
+    })
+}
+
 //menu template
 const mainMenuTemplate = [
     {
     label: 'File',
     submenu:[
+        {
+            label: 'project code',
+            click(){
+                createprojectCodeWindow();
+            }
+        },
         {
             label: 'txt file location'
         },
@@ -44,3 +75,27 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+//mac file button creator
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+//Developer tools if not in production
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
